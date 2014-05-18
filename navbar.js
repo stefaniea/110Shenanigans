3fac89f41d7af35e05bb5620f7661e7756f049e6
Stefanie Alfonso <astef@seas.upenn.edu>
2014-05-18 18:01:38 -0400

    navbar (side) using jPanelMenu.  testing in boilerplate.html

--- navbar.js
+++ navbar.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
-	$(".dropdown_menu").hide();
-	$(".button").click(function(){
-		$(".dropdown_menu").slideToggle();
+
+	$("#menu").hide();
+	var jPM = $.jPanelMenu({
+       keyboardShortcuts: [], //get rid of keyboard shortcuts
+       excludedPanelContent: ".deck-container"
+		
+
 	});
+
+	jPM.on();
+
+
+	$('.slide').css("margin-top", "80px");
+
+
+	/*var jPM = $.jPanelMenu({
+    menu: '#custom-menu-selector',
+    trigger: '.custom-menu-trigger-selector'
+});*/
+
+
+
+
+	/*$(".dropdown_menu ul").hide();
+	$(".menu_wrapper").click(function(){
+		$(this).find(".dropdown_menu ul").get.slideToggle();
+	});*/
 });
