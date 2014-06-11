0ba864b116d1083ec31135e3a2543daa74e725e0
Stefanie Alfonso <astef@seas.upenn.edu>
2014-06-11 12:39:33 -0400

    minor fixes and rearranging code

--- js/script.js
+++ js/script.js
@@ -1,91 +1,87 @@
-$(function() {
-  $.deck('.slide');
-
+  $(function() {
+    $.deck('.slide');    
+    $(parent.top).ready(function() {
   //homework 2 active:
- // top.$("#hw2").style.color = "orange";
+  // top.$("#hw2").style.color = "orange";
 
-  var menu_ul;
-  var menu_children;
+  //var menu_ul;
+  //var menu_children;
   var clicked;
-  var submenu;
-  var open = false;
+  var submenu = document.createElement("div"); //wrapper for table of contents in sidebar
+  submenu.setAttribute("id", "submenu");
+  var open = true; //submenu open/closed
+  var active_homework = top.$(".hw-menu .active li");
 
 
 
 
   try {
     menu_ul = top.$(".hw-menu");
-   // menu_children = menu_ul.getElementsByTagName("*");
- }
- catch(e) {
+  // menu_children = menu_ul.getElementsByTagName("*");
+}
+catch(e) {
   alert("error :/ " + e);
 }
 
-$(parent.top).ready(function() {
 
+createSubmenu(submenu, active_homework);
 
-  top.$(".hw-menu .active li").click(function() {
-
-    
-
-  //remove all submenus
- // top.$(".hw-menu a li").remove("div");
+(active_homework).click(function() {
+  //remove the submenu
   if(open) {
-    submenu.remove();
+    console.log("hiding");
+    //submenu.css("display", "none");
+    //submenu.hide();
+    top.$(".hw-menu .active #submenu").hide();
     open = false;
   } 
-
-else {
+  else {
+  //  submenu.show();
+  console.log("showing");
   open = true;
-    clicked = $(this);
-    submenu = document.createElement("div");
-
- //menu_ul.empty();
- //menu_ul.hide();
-
- $.each($.deck('getSlides'), function(i, $slide) {
-  var id = $slide.attr('id');
-
-      //create link for section using the id
-      var slide_a = document.createElement("a");
-      var slide_li = document.createElement("li");
-      slide_li.style.height = "10px";
-      slide_a.style.height="10px";
-      var slide_header = document.createElement("h6");
-      slide_header.innerHTML = id;
-      slide_a.appendChild(slide_li);
-      slide_li.appendChild(slide_header);
-
-      if (id) {
-        if(id.indexOf("slide") == -1) { /*added this b/c countNested wasn't working.  Instead, checking if slide id has the word slide in it */
-          //clicked.appendChild(slide_a);
-          submenu.appendChild(slide_a);
-         return;
-       }
-     }
-
-   });
-
-    clicked.append(submenu);
+  top.$(".hw-menu .active #submenu").show();
   }
 
-});
+  }); //active hw clicked
+  }); //parent.ready
 
-   top.$(".hw-menu a li").mouseleave(function(){
- // menu_ul.append(menu_children);
- //alert("mouseleave")
-  //document.reload();
-  if(submenu) submenu.remove();
+      function createSubmenu(submenu, active_homework) {
+            open = true;
+            console.log("creating submen");
 
-  
-});
+        /*submenu.css({
+        "background-color": "blue",
+        "font-size": "12",
+        "margin-top": "-50px"
+      });*/
 
+        //menu_ul.empty();
+        //menu_ul.hide();
 
+        $.each($.deck('getSlides'), function(i, $slide) {
+          var id = $slide.attr('id');
 
-});
+        //create link for section using the id of the section
+        var slide_a = document.createElement("a");
+        var slide_li = document.createElement("li");
+        slide_li.style.height = "10px";
+        slide_a.style.height="10px";
+        var slide_header = document.createElement("h6");
+        slide_header.innerHTML = id;
+        slide_a.appendChild(slide_li);
+        slide_li.appendChild(slide_header);
 
+        if (id) {
+          if(id.indexOf("slide") == -1) { /*added this b/c countNested wasn't working.  Instead, checking if slide id has the word slide in it */
+        //clicked.appendChild(slide_a);
+      submenu.appendChild(slide_a);
+      return;
+      }
+      }
 
+      });
 
-
+        active_homework.append(submenu);
+  }
 
 });
